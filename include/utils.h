cb7ba03c1aaf167ba9e057ba63eb8351c8933676
haxeleet <haxeleet@icloud.com>
2024-07-27 07:03:26 +0900

    irq draft

--- include/utils.h
+++ include/utils.h
@@ -2,5 +2,7 @@
 #define __UTILS_H__
 
 int get_el();
+void put32(unsigned long, unsigned int);
+unsigned int get32(unsigned long);
 
 #endif //__UTILS_H__
--- src/main.c
+++ src/main.c
@@ -3,20 +3,21 @@
 #include "uart.h"
 #include "io.h"
 #include "utils.h"
+#include "interrupt.h"
+#include "irq.h"
+#include "timer.h"
 
-
-#ifdef AARCH64
-// arguments for AArch64
 void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
-#else
-	// arguments for AArch32
-void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
-#endif
 {
 	// initialize UART for Raspi2
 	uart_init(3);
 	printf("Hel%co, %s World!\n", 'l', "kernel");
 	printf("Current EL:%d\n", get_el());
+
+	irq_vector_init();
+	timer_init();
+	enable_interrupt_controller();
+	enable_irq();
 
 	while (1)
 		uart_putc(uart_getc());
