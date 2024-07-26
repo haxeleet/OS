cb25dae1f59c7a06aed974e41ca81f89b62db80d
haxeleet <haxeleet@icloud.com>
2024-07-27 06:59:00 +0900

    step by el3 -> 2 -> 1

--- include/sysregs.h
+++ include/sysregs.h
@@ -20,6 +20,8 @@
 
 #define SPSR_MASK_ALL 			(7 << 6)
 #define SPSR_EL1h			(5 << 0)
-#define SPSR_VALUE			(SPSR_MASK_ALL | SPSR_EL1h)
+#define SPSR_EL2h			(9 << 0)
+#define SPSR_EL1_VALUE			(SPSR_MASK_ALL | SPSR_EL1h)
+#define SPSR_EL2_VALUE			(SPSR_MASK_ALL | SPSR_EL2h)
 
 #endif //__SYSREGS_H__
